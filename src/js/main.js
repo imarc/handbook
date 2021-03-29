17639c08b072182a44ff3018eee28fda6fa62b46
Marcel Moreau <marcel@imarc.com>
2021-03-29 00:26:41 -0400

    gh-pages: added light/dark modes

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,21 @@ gsap.from(".entry__item", {
         grid: [0,1],
     }
 });
+
+// Color mode
+const toggleColorMode = function toggleColorMode(e) {
+    if (e.currentTarget.classList.contains("button--color-mode-light")) {
+        document.documentElement.setAttribute("color-mode", "light")
+        localStorage.setItem("color-mode", "light")
+        return
+    }
+
+    document.documentElement.setAttribute("color-mode", "dark");
+    localStorage.setItem("color-mode", "dark")
+};
+
+const toggleColorButtons = document.querySelectorAll(".button--color-mode")
+
+toggleColorButtons.forEach((btn) => {
+    btn.addEventListener("click", toggleColorMode)
+})
