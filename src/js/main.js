c6ab57039b8bc86b5bf01a0ea1468a86592860a1
Marcel Moreau <marcel@imarc.com>
2021-04-02 17:26:29 -0400

    gh-pages: adding stylelint, svg optimizations

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@
 
 import { gsap } from 'gsap'
 import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin'
+import { Flip } from 'gsap/Flip'
 
 gsap.registerPlugin(DrawSVGPlugin)
+gsap.registerPlugin(Flip)
 
 gsap.from(".entry__item", {
     ease: "power3.out",
@@ -26,9 +28,22 @@ tl.from(".icon--dept .icon__part", {
 }, 0.1)
 
 
+
 // Color mode
+
 const toggleColorMode = function toggleColorMode(e) {
-    if (e.currentTarget.classList.contains("button--color-mode-light")) {
+
+    const state = Flip.getState(toggleColorButtonActive)
+
+    e.currentTarget.appendChild(toggleColorButtonActive)
+
+    Flip.from(state, {
+        absolute: true,
+        duration: .25,
+        ease: "expo.inOut",
+    });
+
+    if (e.currentTarget.classList.contains("color-mode__button--light")) {
         document.documentElement.setAttribute("color-mode", "light")
         localStorage.setItem("color-mode", "light")
         return
@@ -38,8 +53,12 @@ const toggleColorMode = function toggleColorMode(e) {
     localStorage.setItem("color-mode", "dark")
 };
 
-const toggleColorButtons = document.querySelectorAll(".button--color-mode")
+const toggleColorButtons = document.querySelectorAll(".color-mode__button")
+const toggleColorButtonActive = document.querySelector(".color-mode__active")
+const toggleColorButtonDark = document.querySelector(".color-mode__button--dark")
 
 toggleColorButtons.forEach((btn) => {
     btn.addEventListener("click", toggleColorMode)
 })
+
+
