96bb2c6718f484c320d7145de49c833dd3df08b3
Marcel Moreau <marcel@imarc.com>
2021-05-17 12:07:18 -0400

    2021-reskin: WIP

--- src/js/components/animations.js
+++ src/js/components/animations.js
@@ -1,6 +1,4 @@
 import { gsap } from "gsap"
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin"
-import { CSSPlugin } from "gsap/CSSPlugin"
 
 /**
  * animations
@@ -9,56 +7,46 @@ import { CSSPlugin } from "gsap/CSSPlugin"
 const entryItems = document.querySelectorAll(".entry__item")
 
 Array.from(entryItems).forEach((entryItem) => {
+    const entryItemWidthHalf = entryItem.offsetWidth / 2;
+    const entryItemHeightHalf = entryItem.offsetHeight / 2;
+
+    const arrShapeLocations = [
+        {
+            x: entryItemWidthHalf,
+            y: -entryItemHeightHalf,
+        },
+        {
+            x: -entryItemWidthHalf,
+            y: entryItemHeightHalf
+        }
+    ]
+
+    let shapes = Array.from(entryItem.querySelectorAll(".entry__shape"))
+
+    shapes.sort((a, b) => 0.5 - Math.random())
+
     entryItem.addEventListener("mouseenter", () => {
-        // console.log('test1')
-        // entryItem.querySelectorAll(".entry__shape")
-        const tri = entryItem.querySelector(".entry__shape--triangle")
+        shapes.forEach((shape, index) => {
+            const location = arrShapeLocations[index]
 
+            gsap.to(shape, {
+                duration: .5,
+                ease: 'expo.inOut',
+                scale: 12,
+                ...location,
+            });
+        })
+    })
 
-        gsap.to(tri, {
-            duration: 1,
-            ease: 'bounce',
-            x: -600
-        });
+    entryItem.addEventListener("mouseleave", () => {
+        shapes.forEach((shape, index) => {
+            gsap.to(shape, {
+                duration: .5,
+                ease: 'expo.inOut',
+                scale: 1,
+                x: 0,
+                y: 0,
+            });
+        })
     })
 })
-
-
-
-
-// var entryItems = gsap.timeline()
-
-// entryItems
-
-// gsap.registerPlugin(DrawSVGPlugin)
-
-// gsap.from(".entry__item", {
-//     ease: "power3.out",
-//     opacity: 0,
-//     duration: 1.25,
-//     y: 20,
-//     stagger: {
-//         amount: 0.5,
-//         grid: [0,1],
-//     }
-// });
-
-// var tl = gsap.timeline()
-
-// tl.from(".icon--dept .icon__part", {
-//     delay: .25,
-//     duration: .75,
-//     drawSVG: 0,
-//     opacity: 0,
-//     ease: "expo.in"
-// }, 0.1)
-
-
-    // .to(".ornaments", { duration: 1, css: { backgroundSize: '70% 70%' } })
-    // .to(".backdrop__shape--squiggles", {duration: 1, x: '100%', y: '-50%' })
-
-
-// background-position-x: 0, 100%;
-// background-position-y: -100%;
-// background-repeat: no-repeat;
-// background-size: 0%;
