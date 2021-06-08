10a08cb7b3d343595be9ed37e9165109764be99f
Marcel Moreau <marcel@imarc.com>
2021-06-08 13:19:14 -0400

    mobile-menu: wip

---
+++ src/js/global/mmenu.js
@@ -0,0 +1,28 @@
+/**
+ * mmenu-light
+ * The "lightweight" version of mmenu
+ */
+
+import "mmenu-light/dist/mmenu-light.js"
+
+document.addEventListener (
+    "DOMContentLoaded", () => {
+        const menu = new MmenuLight(
+            document.querySelector("#js-masthead__nav"),
+            "(max-width: 767px)"
+        )
+
+        const navigator = menu.navigation(
+            {
+                theme: ""
+            }
+        )
+        const drawer = menu.offcanvas()
+
+        document.querySelector( "a[href='#js-masthead__nav']" )
+            .addEventListener( "click", ( evnt ) => {
+                evnt.preventDefault();
+                drawer.open();
+            })
+    }
+)
