fcbd612d277cfb55eb2926f18f9378af8d8ecad9
Marcel Moreau <marcel@imarc.com>
2021-06-09 10:44:32 -0400

    sticky-header: positioning issues with sticky nav

--- src/js/components/stickyTOC.js
+++ src/js/components/stickyTOC.js
@@ -7,6 +7,8 @@ const navTOC = document.querySelector(".nav--toc")
 const stickTOC = () => {
     const viewportHeight = document.documentElement.clientHeight
 
+    // make a const dedicated to navTOC with my 4.5rem masthead height plus 1rem $spacer????
+
     if (navTOC.clientHeight <= viewportHeight) {
         navTOC.classList.add("torso__nav--toc-stuck")
     } else {
